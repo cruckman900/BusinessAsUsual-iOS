70ed8c59c004b285b46ac19f5ccd2d2f792f6f31
Christopher Ruckman <cruckman900@cardinal.wju.edu>
2025-12-29 21:39:09 -0500

    Initial Commit

---
+++ BusinessAsUsualiOS/BusinessAsUsualiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessAsUsualiOSApp.swift
+//  BusinessAsUsualiOS
+//
+//  Created by Christopher Ruckman on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct BusinessAsUsualiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
